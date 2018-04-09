c5c2107543037bbc04a24e8dd55c35291011a7ca
ayala <deus@ubuntu.ubuntu-domain>
2018-04-09 14:47:29 -0700

    add state class

---
+++ state.js
@@ -0,0 +1,34 @@
+class State {
+  constructor() {
+    let total = 0;
+    let unique = 0;
+    let duplicates = 0;
+  }
+
+  getState() {
+    return {
+      total: this.total,
+      unique: this.unique,
+      duplicates: this.duplicates,
+    }
+  }
+  
+  incrementTotal() {
+    this.total += 1;
+  }
+
+  incrementUnique() {
+    this.unique += 1;
+  }
+
+  incrementDuplicates() {
+    this.duplicates += 1;
+  }
+
+  resetTemp() {
+    this.unique = 0;
+    this.duplicates = 0;
+  }
+}
+
+export { State as default }
