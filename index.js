00cd3d7a38a82ee4cd1711711ec12ac1e6e1a44c
ayala <deus@ubuntu.ubuntu-domain>
2018-04-09 13:31:35 -0700

    init socket connection

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const sockets = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+sockets.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('phone number', function(number){
+    console.log('received number:', number)
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(4000, function(){
