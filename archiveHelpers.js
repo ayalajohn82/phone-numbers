bca4819b373b2b0951b3d9444b46d2ef08ac1ffb
ayala <deus@ubuntu.ubuntu-domain>
2018-04-09 14:00:53 -0700

    add insert helper

--- archiveHelpers.js
+++ archiveHelpers.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
 const filePath = './numbers.log';
 
-// If file exists when init it removes file
+/**
+ * If file exists when init it removes file
+ */
 exports.init = function() {
   if (fs.existsSync(filePath)) {
     fs.unlinkSync(filePath);
   }
+}
+
+/**
+ * adds a number to the numbers.log archive
+ * @param {string} number - phone number being added to archive
+ * @returns {boolean} - whether number was added
+ */
+exports.addNumberToList = function(number) {
+  var writable = number + '\n';
+  fs.appendFile('./numbers.log', writable, function(error) {
+    if (error) {
+      console.log('Phone Number Insert Failed', error);
+      return false;
+    }
+    console.log('added number successfully');
+    return true;
+  });
 }
